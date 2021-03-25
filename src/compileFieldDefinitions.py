d35aa17e3305804217ddec281d538ab12f9298f1
Florian Kräutli <florian.kraeutli@uzh.ch>
2021-03-25 13:25:55 +0100

    rename

--- src/compileFieldDefinitions.py
+++ src/compileFieldDefinitions.py
@@ -4,7 +4,7 @@ sys.path.append('/Users/fkraeutli/Sites/sari-field-definitions-generator/src')
 from sariFieldDefinitionsGenerator import generator
 
 inputFile = './fieldDefinitions.yml'
-outputFile = '../data/templates/http%3A%2F%2Frs.swissartresearch.net%2Fpartial%2FfieldDefinitions.html'
+outputFile = '../data/templates/http%3A%2F%2Fstatic.swissartresearch.net%2Fpartial%2FfieldDefinitions.html'
 
 model = generator.loadSourceFromFile(inputFile)
 
