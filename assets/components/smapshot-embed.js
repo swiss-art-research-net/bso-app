539e728c3355fbd4f5f15dfda5fec39282c918af
Florian Kräutli <florian.kraeutli@uzh.ch>
2021-10-13 16:00:44 +0200

    add smapshot view

--- assets/components/smapshot-embed.js
+++ assets/components/smapshot-embed.js
@@ -4,15 +4,17 @@ class SmapshotEmbed extends HTMLElement {
       this._shadowRoot = this.attachShadow({mode: 'open'});
     }
     static get observedAttributes(){
-      return ["iri"];
+      return ["iri", "width", "height"];
     }
     attributeChangedCallback(iri, oldValue, newValue) {
       this._shadowRoot.innerHTML = `<iframe src=https://smapshot.heig-vd.ch/embed/owner/sari/original_image/${newValue}></iframe>`;
     }
     connectedCallback(){
       let iri = this.getAttribute("iri");
+      let width = this.getAttribute("width") || 600;
+      let height = this.getAttribute("height") || 400;
       let id = iri.substr(iri.lastIndexOf('/') + 1);
-      this._shadowRoot.innerHTML = `<iframe src=https://smapshot.heig-vd.ch/embed/owner/sari/original_image/${id}></iframe>`;
+      this._shadowRoot.innerHTML = `<iframe src="https://smapshot.heig-vd.ch/embed/owner/sari/original_image/${id}" width="${width}" height="${height}"></iframe>`;
     }
   }
 
