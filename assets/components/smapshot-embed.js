ddda436d3a9bdcb673cc134449b40a3620ee4549
Florian Kräutli <florian.kraeutli@uzh.ch>
2021-10-18 16:11:50 +0200

    add description

--- assets/components/smapshot-embed.js
+++ assets/components/smapshot-embed.js
@@ -1,3 +1,26 @@
+/**
+ * 
+ * This component integrates the Smapshot (https://smapshot.heig-vd.ch/) embed view
+ * for displaying a georeferenced image in a 3D landscape. The view is embedded as
+ * an iFrame, therefore all functional and visual aspects are controlled by
+ * Smapshot.
+ * 
+ * Usage:
+ * 
+ * <!-- basic usage -->
+ * <smapshot-embed 
+ *  iri="https://resource.swissartresearch.net/artwork/nb-162431">
+ * </smapshot-embed>
+ *
+ * 
+ * <!-- width optional width and height attributes -->
+ * <smapshot-embed 
+ *  iri="https://resource.swissartresearch.net/artwork/nb-162431" 
+ *  width="100%"
+ *  height="100%">
+ * </smapshot-embed>
+ * 
+ */
 class SmapshotEmbed extends HTMLElement {
     constructor() {
       super();
@@ -7,7 +30,8 @@ class SmapshotEmbed extends HTMLElement {
       return ["iri", "width", "height"];
     }
     attributeChangedCallback(iri, oldValue, newValue) {
-      this._shadowRoot.innerHTML = `<iframe src=https://smapshot.heig-vd.ch/embed/owner/sari/original_image/${newValue}></iframe>`;
+      let id = newValue.substr(newValue.lastIndexOf('/') + 1);
+      this._shadowRoot.innerHTML = `<iframe src=https://smapshot.heig-vd.ch/embed/owner/sari/original_image/${id}></iframe>`;
     }
     connectedCallback(){
       let iri = this.getAttribute("iri");
