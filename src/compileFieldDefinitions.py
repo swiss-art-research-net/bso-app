03dcd32bf43ee1d1085184495e8dad3f46be44dd
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-01-25 14:08:22 +0100

    add localisations

--- src/compileFieldDefinitions.py
+++ src/compileFieldDefinitions.py
@@ -6,9 +6,22 @@ from sariFieldDefinitionsGenerator import generator
 inputFile = './fieldDefinitions.yml'
 outputFile = '../data/templates/https%3A%2F%2Fstatic.swissartresearch.net%2Fpartial%2FfieldDefinitions.html'
 
+def addLocalisation(jsonString, bundle="bso"):
+    import re
+
+    def replaceWithLocalised(match):
+        key = "field_" + re.sub(r'[\W\s]', '_', match.group(1)).lower()
+        return '"label": "[[i18n "' + key + '" bundle="' + bundle + '"]]"'
+
+    pattern = r'"label": "(.*)"'
+    return re.sub(pattern, replaceWithLocalised, jsonString)
+
+
 model = generator.loadSourceFromFile(inputFile)
 
 output = generator.generate(model, generator.JSON)
+
+output = addLocalisation(output)
 
 with open(outputFile, 'w') as f:
     f.write(output)
